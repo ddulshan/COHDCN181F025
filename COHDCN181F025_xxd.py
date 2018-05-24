895a35779f68465e64ea3241fdf6e9ea782ac5f2
ddulshan <ddulshan100@gmail.com>
2018-05-25 00:52:18 +0530

    Add files via upload

--- COHDCN181F025_xxd.py
+++ COHDCN181F025_xxd.py
@@ -1,42 +1,42 @@
 import argparse
+import binascii
+
+def chunk(string):
+    return(string[c:c+4] for c in range(0, len(string), 4))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--file", "-f", type = str, required = True)
 args = parser.parse_args()
 
 no = 0
-read = 2
-
+read = 0
 line = 0
 length = 0
 
 try:
-    file = open(args.file, "rb")
+    file = open(args.file, "r", errors='ignore')
 
 except IOError:
     print("File not found")
     quit()
 
-    rawContent = file.read()
-    strContent = str(rawContent)
-    strContent = strContent.replace(" ", ".")
-    strContent = strContent[:len(strContent)-1]
+rawContent = file.read()
+strContent = rawContent.replace(" ", ".")
+hexContent = (binascii.hexlify(rawContent.encode("utf8"))).decode("utf8")
 
-    length = int(len(strContent))
-    if length < 16:
-        line = 1
-    else:
-        line = length/16 +1
-
-    for c in range(int(line)):
-        text = "{:#08x}".format(no) + " | "
-        text += " ".join("{:02x}".format(ord(x)) \
-            for x in strContent[read:read+16])
-        text += " | " + strContent[read:read+16]
-        no += 16
-        read += 16
-
-        print(text)
-
+length = int(len(rawContent))
+if length < 16:
+    line = 1
 else:
-    print("No file found")
+    line = length/16 + 1
+
+for c in range(int(line)):
+    text = "{:#08x}".format(no) + " | "
+    text += " ".join(chunk(hexContent[read:read+32]))
+    text += " | " + strContent[read:read+16]
+    no += 16
+    read += 16
+
+    print(text)
+
+
