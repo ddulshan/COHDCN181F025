c90cbd98c75424a9eaac54a687d26e93727c3fcf
ddulshan <ddulshan100@gmail.com>
2018-05-27 19:52:53 +0530

    Add files via upload

---
+++ COHDCN181F025_tcp.py
@@ -0,0 +1,34 @@
+from ctypes import *
+import struct
+import sys
+import os
+import socket
+
+class TCP(Structure):
+    _fields_=[
+        ("src", c_ushort),
+        ("dest", c_ushort),
+        ("seq_no", c_long),
+        ("ack_no", c_long),
+        ("offset", c_ubyte, 4),
+        ("reserved", c_ubyte, 4),
+        ("flag",  c_ubyte),
+        ("window", c_ushort),
+        ("checksum", c_ushort),
+        ("urg_point", c_ushort),
+        ]
+
+    def __new__(self, socket_buffer = None):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer = None):
+        self.src_port =  struct.unpack("<H", struct.pack(">H", self.src))
+        self.dest_port =  struct.unpack("<H", struct.pack(">H", self.dest))
+
+proto = {1:"ICMP", 2:"IGMP", 6:"TCP"}
+
+rawfile = open("tcp_frame.bin", "rb").read()
+
+tcp = TCP(rawfile)
+print("Source Port: " + ''.join(map(str, tcp.src_port)))
+print("Destination Port: " + ''.join(map(str, tcp.dest_port)))
---
+++ COHDCN181F025_udp.py
@@ -0,0 +1,26 @@
+from ctypes import *
+import struct
+import sys
+import os
+import socket
+
+class UDP(Structure):
+    _fields_=[
+        ("src", c_ushort),
+        ("dest", c_ushort),
+        ("length", c_short),
+        ("checksum", c_short),
+        ]
+
+    def __new__(self, socket_buffer = None):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer = None):
+        self.src_port =  struct.unpack("<H", struct.pack(">H", self.src))
+        self.dest_port =  struct.unpack("<H", struct.pack(">H", self.dest))
+
+rawfile = open("udp_frame.bin", "rb").read()
+
+udp = UDP(rawfile)
+print("Source Port: " + ''.join(map(str, udp.src_port)))
+print("Destination Port: " + ''.join(map(str, udp.dest_port)))
